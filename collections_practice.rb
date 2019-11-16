83d79e557002d44044badb2254e976a026e485da
Uma Manandhar <um_2003_8@hotmail.com>
2019-11-16 18:38:29 +0000

    Done.

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,1 +1,53 @@
+def sort_array_asc(array)
+  array.sort
 
+end
+
+def sort_array_desc(array)
+  array.sort {|a, b| b <=> a}
+
+end
+
+def sort_array_char_count(array)
+  array.sort {|a, b| a.size <=> b.size}
+
+end
+
+def swap_elements(array)
+
+  array[1], array[2] = array[2], array[1]
+  array
+
+end
+
+def reverse_array(array)
+  array.reverse
+
+end
+
+def kesha_maker(array)
+  array.each {|letter| letter[2] = "$"}
+  array
+
+end
+
+def find_a(array)
+  array.select {|word| word.start_with? "a"}
+
+end
+
+def sum_array(array)
+  array.sum
+
+end
+
+def add_s(array)
+  array.each_with_index.collect do|element, index|
+    if index == 1
+      element
+    else
+      element + "s"
+    end
+  end
+
+end
